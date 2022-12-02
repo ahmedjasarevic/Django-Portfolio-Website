ad39adf10cb33fed777944f009854f3f0b6f1721
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-12-02 22:27:49 +0100

    a

--- main/urls.py
+++ main/urls.py
@@ -20,9 +20,5 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("register/", views.register, name="register"),
-    path("logout/", views.logout_request, name="logout"),
-    path("login/", views.login_request, name="login"),
-    path("<single_slug>", views.single_slug, name="single_slug"),
-    path("account/", views.view_profile, name="profile"),
+    path("contact/", views.contact, name="contact"),
 ]
