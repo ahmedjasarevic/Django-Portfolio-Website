42a6b2fae7dae929fde2a382a732e45db13a0929
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-12-05 19:59:28 +0100

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -149,7 +149,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS=[
+   os.path.join(BASE_DIR, "static"),
+    '/Github/Django-Portfolio-Website/main/static/'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
