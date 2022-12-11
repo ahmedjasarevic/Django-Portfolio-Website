bef2706f82302e02a46bfcfc188b5eeffd68f765
Ahmed Jasarevic <jasarevicahmed999@gmail.com>
2022-12-11 19:03:44 +0100

    a

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,9 +13,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
